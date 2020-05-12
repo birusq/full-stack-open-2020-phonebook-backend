e3fb0de45edf31ac2ce07b12fe02b03203959402
Eero Lehtinen <eero@eerolehtinen.fi>
2020-05-12 05:47:59 +0300

    heroku

---
+++ build/precache-manifest.de5aa180fb28c7e8fe5d7d9166ca8a6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f0bc5bc4dc7ec17ed6d84d51ed0638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "489a133cbad74e9e6d1c",
+    "url": "/static/css/main.4c309ffb.chunk.css"
+  },
+  {
+    "revision": "58bd7345623f9bd76703",
+    "url": "/static/js/2.e3d3a486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3d3a486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "489a133cbad74e9e6d1c",
+    "url": "/static/js/main.669ca2f9.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
