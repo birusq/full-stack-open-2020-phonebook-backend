60ccedbd6bed7a7f8b23c95bef84156455882c59
Eero Lehtinen <eero@eerolehtinen.fi>
2020-05-13 03:07:38 +0300

    uibuild

---
+++ build/precache-manifest.9d51ec40c7e01c03318b56218185dbac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c16d28e16cf64a008e141f272b40423",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bea3dfbde0d4b836c6f8",
+    "url": "/static/css/main.073da8de.chunk.css"
+  },
+  {
+    "revision": "58bd7345623f9bd76703",
+    "url": "/static/js/2.e3d3a486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3d3a486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bea3dfbde0d4b836c6f8",
+    "url": "/static/js/main.306f3812.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
